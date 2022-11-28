b861e399fae60eca4798207402477910267ca3e9
Charita Malhotra <63345140+CharitaMalhotra@users.noreply.github.com>
2022-11-28 12:34:29 +0530

    Update streamlit_app.py
    We'll ask our app to put the list of selected fruits into a variable called fruits_selected. Then, we'll ask our app to use the fruits in our fruits_selected list to pull rows from the full data set (and assign that data to a variable called fruits_to_show). Finally, we'll ask the app to use the data in fruits_to_show in the dataframe it displays on the page.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,23 @@ import streamlit
 # streamlit.title("I got selected in my dream company-GOOGLE 🫶🥰")
 streamlit.header("I got selected in my dream company-GOOGLE 🫶🥰")
 streamlit.header("🇪🇺 PARIS IS BEAUTIFUL 🗼")
+#########################################################################################################
 
 import pandas
-my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list=my_fruit_list.set_index('Fruit')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# we'll pre-populate the list to set an example for the customer. 
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado","Lemon"])
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-# we'll pre-populate the list to set an example for the customer. 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado","Lemon"])
-
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(my_fruit_list)
+
+#########################################################################################################
+fruits_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index)
+fruits_to_show = my_fruits_list.loc[fruits_slected]
+                                      
+streamlit.dataframe(fruits_to_show)
