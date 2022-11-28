dee13f81b6887344b7717d08f30c7b109ef1c065
Charita Malhotra <63345140+CharitaMalhotra@users.noreply.github.com>
2022-11-28 11:27:10 +0530

    Update streamlit_app.py
    reading a csv file through pandas and displaying on streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
-streamlit.title("I got selected in my dream company-GOOGLE 🫶🥰")
+# streamlit.title("I got selected in my dream company-GOOGLE 🫶🥰")
+streamlit.header("I got selected in my dream company-GOOGLE 🫶🥰")
 streamlit.header("🇪🇺 PARIS IS BEAUTIFUL 🗼")
+
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
